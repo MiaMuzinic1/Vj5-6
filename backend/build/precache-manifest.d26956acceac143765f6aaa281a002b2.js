37f4ba1e50d78b9ce04915303ce931f48e3068af
Mia Muzinic <mmuzinic@pmfst.hr>
2021-11-21 20:15:22 +0100

    Dodano sve

---
+++ backend/build/precache-manifest.d26956acceac143765f6aaa281a002b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5cec047902d445bc474cc83d95d2d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557b13355f0d507aec06",
+    "url": "/static/css/main.a5670662.chunk.css"
+  },
+  {
+    "revision": "43b6cea0238e39ea0840",
+    "url": "/static/js/2.cebc3bdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cebc3bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557b13355f0d507aec06",
+    "url": "/static/js/main.5a2d37c8.chunk.js"
+  },
+  {
+    "revision": "b5942b035685b24b37e7",
+    "url": "/static/js/runtime-main.0a493ba8.js"
+  }
+]);
